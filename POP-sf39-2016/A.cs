1cd6067da280d4f65a1bb6c9128c60f712bc4ea9
student <student@ftn.ns>
2017-10-16 12:27:49 +0200

    init

---
+++ POP-sf39-2016/A.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prostorImena
+{
+    class A
+    {
+        private string naziv;
+        private string ime;
+
+        public string GetNaziv()
+        {
+            return this.naziv;
+        }
+        public void SetNaziv(string naziv)
+        {
+            this.naziv = naziv;
+        }
+        public string Ime
+        {
+            get
+            {
+                return this.ime;
+            }
+            set
+            {
+                this.ime = value;
+            }
+        }
+    }
+
+}
---
+++ POP-sf39-2016/Tests/Test1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POP_sf39_2016.Tests
+{
+    class Test1
+    {
+    }
+}
